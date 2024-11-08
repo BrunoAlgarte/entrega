c6601ea172d09b678adc3ac64beb6aabb8f330d1
BrunoAlgarte <brunoalgter@gmail.com>
2024-11-08 20:10:35 -0300

    Primeiro commit

---
+++ app/app.js
@@ -0,0 +1,16 @@
+// Importa o módulo express
+const express = require('express');
+const app = express();
+
+// Define uma rota GET para o endpoint "/"
+app.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+// Define a porta do servidor
+const PORT = 8083;
+
+// Inicia o servidor
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
